aa56a48b0aa5678682c63ce592b9461930300b65
Aleksandar_Milutinovic <63-2020@pmf.kg.ac.rs>
2025-02-26 18:17:08 +0100

    #16 completed the ui for registration

--- client/src/app/auth/ui/signup/Signup.tsx
+++ client/src/app/auth/ui/signup/Signup.tsx
@@ -56,46 +56,75 @@ const Signup = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-        <form onSubmit={handleSubmit} className="w-80 space-y-4">
-          <InputText
-            type="text"
-            name="username"
-            placeholder="Full Name"
-            value={user.username}
-            onChange={handleChange}
-            required
-          />
-          <InputText
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={user.email}
-            onChange={handleChange}
-            required
-          />
-          <InputText
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={user.password}
-            onChange={handleChange}
-            required
-          />
-          <Button type="submit" value="Sign up" onClick={handleSubmit} />
-        </form>
-        <p className="mt-4">
-          Already have an account?{" "}
-          <span
-            className="text-blue-500 cursor-pointer"
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </span>
-        </p>
-      </div>
-      <Toast ref={toast} />
+      <body className="d-flex justify-content-center min-vw-100 vw-100 container">
+        <div className="row">
+          <div className="col d-flex align-items-center p-0 justify-content-center mw-25 mh-50">
+            <img
+              src="../../../src/assets/SignInBackGround.svg"
+              alt="des"
+              class="w-100 h-100 object-fit-cover"
+            />
+          </div>
+          <div className="col d-flex  flex-column p-0 shadow">
+            <div class="w-80 mx-auto my-auto">
+              <div className="d-flex flex-row mx-">
+                <img src="../../../src/assets/SmartGardenLogo.svg" alt="des" />
+                <h3>Smart Garden</h3>
+              </div>
+
+              <h2 className="text-2xl font-bold mt-3">Create an account</h2>
+              <h6 className="">
+                Already have an account?{" "}
+                <span
+                  className="text-success cursor-pointer"
+                  onClick={() => navigate("/login")}
+                >
+                  Sign in
+                </span>
+              </h6>
+              <form
+                onSubmit={handleSubmit}
+                className="w-80 d-flex flex-column justify-space-between gap-3 "
+              >
+                <InputText
+                  type="text"
+                  name="username"
+                  placeholder="Full Name"
+                  value={user.username}
+                  onChange={handleChange}
+                  required
+                />
+                <InputText
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={user.email}
+                  onChange={handleChange}
+                  required
+                />
+                <InputText
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={user.password}
+                  onChange={handleChange}
+                  required
+                />
+                <button
+                  type="button"
+                  value="Sign up"
+                  onClick={handleSubmit}
+                  class="btn btn-outline-success w-100 mx-auto"
+                >
+                  Register
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <Toast ref={toast} />
+      </body>
     </>
   );
 };
